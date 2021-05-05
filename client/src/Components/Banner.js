b1815878bfef4063682570469e9680d6c8d4b2ee
Chirag Khandhar <ckhandhar@hawk.iit.edu>
2021-05-05 10:27:49 -0500

    Add Page Banner

---
+++ client/src/Components/Banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "../Styles/Banner.css";
+
+// MUI Stuff
+import Typography from "@material-ui/core/Typography";
+
+function Banner() {
+  return (
+    <div className="page-banner">
+      <img
+        className="pb-img"
+        src="https://source.unsplash.com/random"
+        alt="banner image"
+      />
+      <div className="pb-overlay">
+        <p className="pb-title">
+          Discover the world and plan the perfect trip.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -3,12 +3,19 @@ import "../Styles/HomePage.css";
 
 // Components
 import Navbar from "../Components/Navbar";
+import Banner from "../Components/Banner";
+
+// MUI Stuff
+
+// Icons
+import { FaBeer } from "react-icons/fa";
 
 export class HomePage extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Banner />
       </div>
     );
   }
