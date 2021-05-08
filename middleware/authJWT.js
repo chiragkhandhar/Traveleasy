2f3700a63dbbb29a09938e37fb7ea1384c3335d4
Chirag Khandhar <ckhandhar@hawk.iit.edu>
2021-05-07 20:47:16 -0500

    Update Middleware

--- middleware/authJWT.js
+++ middleware/authJWT.js
@@ -2,19 +2,25 @@ const jwt = require("jsonwebtoken");
 const config = require("../utils/auth.config");
 
 exports.verifyToken = (request, response, next) => {
+  console.log(request.headers.authorization);
   if (request.headers.authorization === undefined)
     return response.status(400).json({ error: "Authorization required" });
-    
-  let token = request.headers.authorization.split("Bearer ")[1];
 
-  if (!token) {
-    return response.status(403).json({ error: "No token provided" });
-  }
-  jwt.verify(token, config.secret, (err, decode) => {
-    if (err) {
-      return response.status(401).json({ error: "Unauthorized Request" });
-    }
-    request.user_id = decode.id;
+  if (request.headers.authorization === "Bearer null") {
+    request.user_id = false;
     next();
-  });
+  } else {
+    let token = request.headers.authorization.split("Bearer ")[1];
+
+    if (!token) {
+      return response.status(403).json({ error: "No token provided" });
+    }
+    jwt.verify(token, config.secret, (err, decode) => {
+      if (err) {
+        return response.status(401).json({ error: "Unauthorized Request" });
+      }
+      request.user_id = decode.id;
+      next();
+    });
+  }
 };
--- routes/places.js
+++ routes/places.js
@@ -1,6 +1,6 @@
 const { response } = require("express");
 const { client_id, client_secret } = require("./api.config");
-const axios = require("axios")
+const axios = require("axios");
 const User = require("../models/user.model");
 
 exports.getPlacesByLatLong = (request, response) => {
@@ -9,120 +9,117 @@ exports.getPlacesByLatLong = (request, response) => {
   axios
     .get(URL)
     .then((res) => {
-      response.status(200).send(res.data)
+      response.status(200).send(res.data);
     })
     .catch((err) => {
       response
         .status(400)
-        .send({ error: "Cannot Fetch Places for this location"+ err });
+        .send({ error: "Cannot Fetch Places for this location" + err });
     });
 };
 
-exports.getPlacesByLocationName= (request, response) => {
+exports.getPlacesByLocationName = (request, response) => {
   const near = request.params.near;
   URL = `https://api.foursquare.com/v2/venues/search?near=${near}&client_id=${client_id}&client_secret=${client_secret}&v=20210504`;
   axios
     .get(URL)
     .then((res) => {
-      response.status(200).send(res.data)
+      response.status(200).send(res.data);
     })
     .catch((err) => {
       response
         .status(400)
-        .send({ error: "Cannot Fetch Places for this location"+ err });
+        .send({ error: "Cannot Fetch Places for this location" + err });
     });
 };
 
-exports.getPlacesByNameCategory= (request, response) => {
+exports.getPlacesByNameCategory = (request, response) => {
   const near = request.params.near;
   const query = request.params.query;
-  console.log(near,query);
+  console.log(near, query);
   URL = `https://api.foursquare.com/v2/venues/search?near=${near}&query=${query}&client_id=${client_id}&client_secret=${client_secret}&v=20210504`;
   axios
     .get(URL)
     .then((res) => {
-      response.status(200).send(res.data)
+      response.status(200).send(res.data);
     })
     .catch((err) => {
       response
         .status(400)
-        .send({ error: "Cannot Fetch Places for this location"+ err });
+        .send({ error: "Cannot Fetch Places for this location" + err });
     });
 };
 
-exports.getPlacesByLatLongCategory= (request, response) => {
+exports.getPlacesByLatLongCategory = (request, response) => {
   const ll = request.params.ll;
   const query = request.params.query;
-  console.log(near,query);
+  console.log(near, query);
   URL = `https://api.foursquare.com/v2/venues/search?ll=${ll}&query=${query}&client_id=${client_id}&client_secret=${client_secret}&v=20210504`;
   axios
     .get(URL)
     .then((res) => {
-      response.status(200).send(res.data)
+      response.status(200).send(res.data);
     })
     .catch((err) => {
       response
         .status(400)
-        .send({ error: "Cannot Fetch Places for this location"+ err });
+        .send({ error: "Cannot Fetch Places for this location" + err });
     });
 };
 
-
-exports.getVenueDetailsByID = (request,response) => {
+exports.getVenueDetailsByID = (request, response) => {
   const VENUE_ID = request.params.id;
   const user_id = request.user_id;
-  URL = `https://api.foursquare.com/v2/venues/${VENUE_ID}?client_id=${client_id}&client_secret=${client_secret}&v=20210504`
-  axios.get(URL)
-    .then((res)=>{
-     const data = res.data;
-     const category = res.data.response.venue.categories[0].shortName;
-     console.log(category);
-     if(!request.user_id){
-      response.status(200).json(data)
+  console.log("User_ID" + user_id);
+  URL = `https://api.foursquare.com/v2/venues/${VENUE_ID}?client_id=${client_id}&client_secret=${client_secret}&v=20210504`;
+  axios.get(URL).then((res) => {
+    const data = res.data;
+    const category = res.data.response.venue.categories[0].shortName;
+    console.log(category);
+    if (!request.user_id) {
+      response.status(200).json(data);
     }
-     User.findOne({_id:user_id})
-      .then(user=>{
-        const browsedcategories = user.browsedcategories;
-        if(browsedcategories.length===0){
-          console.log(category)
-          browsedcategories.push({"category":category,"count":1})
-          User.updateOne(
-            {_id:user_id},
-            {
-              $set:{
-                browsedcategories:browsedcategories
-            }
-          })
-          .then(res=>{
+    User.findOne({ _id: user_id }).then((user) => {
+      const browsedcategories = user.browsedcategories;
+      if (browsedcategories.length === 0) {
+        console.log(category);
+        browsedcategories.push({ category: category, count: 1 });
+        User.updateOne(
+          { _id: user_id },
+          {
+            $set: {
+              browsedcategories: browsedcategories,
+            },
+          }
+        ).then((res) => {
+          response.status(200).json(data);
+        });
+      } else {
+        let temp = browsedcategories.find((e) => e.category === category);
+        if (temp) {
+          temp.count += 1;
+        } else {
+          browsedcategories.push({ category: category, count: 1 });
+        }
+        User.updateOne(
+          { _id: user_id },
+          {
+            $set: {
+              browsedcategories: browsedcategories,
+            },
+          }
+        )
+          .then((res) => {
             response.status(200).json(data);
           })
-        }
-        else{
-            let temp = browsedcategories.find(e => e.category === category)
-            if (temp) { 
-              temp.count+= 1
-            }
-            else{
-              browsedcategories.push({"category":category,"count":1})
-            }
-            User.updateOne(
-              {_id:user_id},
-              {
-                $set:{
-                  browsedcategories:browsedcategories
-              }
-            })
-            .then(res=>{
-              response.status(200).json(data);
-            })
-            .catch(err=>{
-              response.status(200).send({"error":"Request Cannot be completed"});
-            })
-        }
-      })
-    })
-}
+          .catch((err) => {
+            response.status(200).send({ error: "Request Cannot be completed" });
+          });
+      }
+    });
+  });
+};
 
 exports.sayHello = (request, response) => {
-  response.status(200).json({"message":"hello world"})
-  };
+  response.status(200).json({ message: "hello world" });
+};
